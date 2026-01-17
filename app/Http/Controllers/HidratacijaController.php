2e7798d38efa9f51efca5586f1a514fe791d9e69
Sofija <gutaljsofija@gmail.com>
2026-01-17 21:41:39 +0100

    Kreirane API rute i controlleri kao i vraćanje podataka u json formatu

---
+++ app/Http/Controllers/HidratacijaController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Hidratacija;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class HidratacijaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Hidratacija::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator= Validator::make($request->all(),[
+            'korisnik_id'   => 'required|integer|exists:users,id',
+            'datum'         => 'required|date',
+            'cilj_mi'       => 'required|integer',
+            'uneseno_mi'    => 'required|integer',
+            'created_at'        => 'required|date',
+            'updated_at'        => 'required|date',
+      ]);
+
+        if($validator->fails()){
+            return response()->json([
+        'message'=> 'Validacija nije prosla',
+        'errors'=> $validator->errors(),
+            ],422);
+        }
+        $data=$validator->validated();
+        $hidratacija=Hidratacija::create($data);
+        return response()->json($hidratacija,201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+         return Hidratacija::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Hidratacija $hidratacija)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request,$id)
+    {   
+        $hidratacija=Hidratacija::find($id);
+
+        if(!$hidratacija){
+            return response()->json(['message'=>'Hidratacija nije pronadjena.'],404);
+        }
+        $validator= Validator::make($request->all(),[
+            'korisnik_id'   => 'required|integer|exists:users,id',
+            'datum'         => 'required|date',
+            'cilj_mi'       => 'required|integer',
+            'uneseno_mi'    => 'required|integer',
+            'created_at'        => 'required|date',
+            'updated_at'        => 'required|date',
+
+      ]);
+
+   if($validator->fails()){
+    return response()->json([
+   'message'=> 'Validacija nije prosla',
+   'errors'=> $validator->errors(),
+    ],422);
+   }
+   $data=$validator->validated();
+   //$hidratacija->update($data);
+   return response()->json($hidratacija,200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $hidratacija=Hidratacija::find($id);
+
+        if(!$hidratacija){
+            return response()->json(['message'=>'Hidratacija nije pronadjena.'],404);
+        }
+        $hidratacija->delete;
+        return response()->json(['message'=>'Hidratacija je obrisana.'],200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\VezbaController;
+use App\Http\Controllers\HidratacijaController;
+use App\Http\Controllers\TreningController;
+use App\Models\DnevnikIshrane;
 
 // 🔐 Javni endpointi (bez logovanja)
 Route::post('/register', [AuthController::class, 'register']);
@@ -24,3 +27,22 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::delete('/vezbe/{id}', [VezbaController::class, 'destroy']);
     });
 });
+Route::get('/treninzi/{id}',[TreningController::class, 'show']);
+Route::get('/treninzi',[TreningController::class, 'index']);
+Route::post('/treninzi',[TreningController::class, 'store']);
+Route::delete('/treninzi/{id}',[TreningController::class, 'destroy']);
+Route::put('/treninzi/{id}',[TreningController::class, 'update']);
+
+Route::get('/hidratacije/{id}',[HidratacijaController::class, 'show']);
+Route::get('/hidratacije',[HidratacijaController::class, 'index']);
+Route::post('/hidratacije',[HidratacijaController::class, 'store']);
+Route::delete('/hidratacije/{id}',[HidratacijaController::class, 'destroy']);
+Route::put('/hidratacije/{id}',[HidratacijaController::class, 'update']);
+
+Route::get('/dnevnici_ishrane/{id}',[DnevnikIshrane::class, 'show']);
+Route::get('/dnevnici_ishrane',[DnevnikIshrane::class, 'index']);
+Route::post('/dnevnici_ishrane',[DnevnikIshrane::class, 'store']);
+Route::delete('/dnevnici_ishrane/{id}',[DnevnikIshrane::class, 'destroy']);
+Route::put('/dnevnici_ishrane/{id}',[DnevnikIshrane::class, 'update']);
+
+
